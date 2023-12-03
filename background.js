e30a02584e1c69d39c74c150d28ce3ec9ad6c02b
1minimi <xnfmzmwhrsla@gmail.com>
2023-12-03 18:28:27 +0900

    더미 테스트

--- background.js
+++ background.js
@@ -3,6 +3,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       var activeTab = tabs[0];
       console.log('tabs' + tabs);
+      console.log('tabs' + tabs === null);
       console.log('activeTab:' + activeTab);
       // 디버깅하면 tabs가 제대로 안넘어옴, (단 동작은 제대로함)
       // manifest에 contentscript 넣어서 자동 inject될 때만 디버깅 했을 때 제대로 넘어오는거 같음
